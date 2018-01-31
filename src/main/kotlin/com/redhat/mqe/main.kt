6a7393e51cb142b000f200f0943951890ce59ef6
Jiri Danek <jdanek@redhat.com>
2018-01-31 14:28:43 +0100

    New Main. Now this is a Gradle land!

--- src/main/kotlin/com/redhat/mqe/main.kt
+++ src/main/kotlin/com/redhat/mqe/main.kt
@@ -1,23 +1,32 @@
 package com.redhat.mqe
 
+import com.redhat.mqe.archiving.TarBz2Writer
+import com.redhat.mqe.archiving.ZipWriter
+import com.redhat.mqe.archiving.compressTestResults
+import com.redhat.mqe.broker.Executor
+import com.redhat.mqe.broker.Printer
+import com.redhat.mqe.broker.TestRunCommands
 import java.nio.file.Paths
 
 fun main(args: Array<String>) {
     when (args[0]) {
-        "runTests" -> runTests()
-        "compressJUnit" -> compressJUnit()
-        else -> throw IllegalArgumentException()
+        "runBrokerSystemTests" -> runBrokerSystemTests()
+        "compressJUnit" -> compressJUnit(args.slice(1..args.size))
+        else -> throw IllegalArgumentException("Specified operation is not implemented")
     }
 }
 
+// 6 hours for integration tests?!?
+// aio/nio, install the lib? I guess so
+//  there is a test to check if aio can be loaded that fails if libaio is not installed
 // reduce the output size
 //  compress right away
 //  print only failing outputs
 // monitor
 //  leaked treads
 //  memory usage for every test, could be better done in JUnit itself through rules, maybe
-//  i am closing...
-fun runTests() {
+//  i am closing... messages
+fun runBrokerSystemTests() {
     val r = TestRunCommands()
     val executor = Executor(r)
     r.listeners.add(Printer())
@@ -29,8 +38,14 @@ fun runTests() {
 
 }
 
-fun compressJUnit() {
-    val archiver = TarBz2Writer(Paths.get("tests.tar.bz2"))
-    compressTestResults(Paths.get("."), archiver)
+fun compressJUnit(args: List<String>) {
+    val resultsDirectory = Paths.get(args[0])
+    val archivePath = Paths.get(args[1])
+    val archiver = when {
+        archivePath.endsWith(".xip") -> ZipWriter(archivePath)
+        archivePath.endsWith(".tar.bz2") -> TarBz2Writer(archivePath)
+        else -> throw IllegalArgumentException("Only .zip and .tar.bz2 archive file extensions are supported")
+    }
+    compressTestResults(resultsDirectory, archiver)
     archiver.close()
 }
